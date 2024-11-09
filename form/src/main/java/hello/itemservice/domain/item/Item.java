36b9d4d9fe2c95fef62d0fd3098bbd1436be8fb2
shawnchoi8 <bychoise8@gmail.com>
2024-11-09 20:34:28 +0900

    Adding requirements

--- form/src/main/java/hello/itemservice/domain/item/Item.java
+++ form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,13 +2,19 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
-
     private Long id;
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    private Boolean open; //판매 여부 (Sale status)
+    private List<String> regions; //판매 지역 (Sales regions)
+    private ItemType itemType; //상품 종류 (Item type)
+    private String deliveryCode; //배송 방식 (Delivery method)
 
     public Item() {
     }
