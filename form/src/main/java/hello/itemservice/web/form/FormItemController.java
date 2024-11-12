4de99f9ff681f3c027429c695b57b9ea18614aae
shawnchoi8 <bychoise8@gmail.com>
2024-11-12 11:13:08 +0900

    Select Box

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,6 +37,15 @@ public class FormItemController {
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
         return ItemType.values(); //ENUM Data in array
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("EXPRESS", "Express Delivery")); //빠른
+        deliveryCodes.add(new DeliveryCode("STANDARD", "Standard Delivery")); //일반
+        deliveryCodes.add(new DeliveryCode("ECONOMY", "Economy Delivery")); //느린
+        return deliveryCodes;
     }
 
     @GetMapping
